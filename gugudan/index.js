c0dda993f5a9dad71e3a6a628cad9b2f0c0bbd9c
leehi0210 <leehi0210@gmail.com>
2020-06-16 23:21:52 +0900

    Add alert for error message

--- gugudan/index.js
+++ gugudan/index.js
@@ -1,6 +1,17 @@
 document.querySelector('#click').addEventListener('click', () => {
 	const first = document.querySelector('#first').value;
 	const second = document.querySelector('#second').value;
+	var err = '숫자를 입력하세요!';
 
-	document.querySelector('#result').textContent = first * second;
+	if (first){
+		if (second){
+			document.querySelector('#result').textContent = first * second;
+		}
+		else {
+			alert(err);
+		}
+	}
+	else {
+		alert(err);
+	}
 });
