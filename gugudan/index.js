94fd091e42a04d9bae4f9801a984c97c95f162d5
leehi0210 <leehi0210@gmail.com>
2020-06-16 23:26:27 +0900

    Add querySelector for error message

--- gugudan/index.js
+++ gugudan/index.js
@@ -1,14 +1,14 @@
 document.querySelector('#click').addEventListener('click', () => {
 	const first = document.querySelector('#first').value;
 	const second = document.querySelector('#second').value;
-	var err = '숫자를 입력하세요!';
+	var err = '숫자를 입력하세요';
 
 	if (first){
 		if (second){
-			document.querySelector('#result').textContent = first * second;
+			document.querySelector('#result').textContent = first * second; first * second;
 		}
 		else {
-			alert(err);
+			document.querySelector('#result').textContent = '두 번째 숫자를 입력하세요';
 		}
 	}
 	else {
