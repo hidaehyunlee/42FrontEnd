0999eef06374f18d449965d2eb7f71f658ffd917
leehi0210 <leehi0210@gmail.com>
2020-06-17 11:51:57 +0900

    Make wordChain game

--- wordchain/index.js
+++ wordchain/index.js
@@ -1,5 +1,23 @@
 const btn = document.querySelector('#btn');
 
-btn.addEventListener('click', () => {
+btn.addEventListener('enter', () => {
 	const word = document.querySelector('#word').textContent;
+	const input = document.querySelector('#input').value;
+	const lastLetter = word.substr(-1);
+	const firstLetter = input[0];
+	const err = '글자를 입력하세요!'
+
+	if (input){
+		if (lastLetter === firstLetter){
+			document.querySelector('#word').textContent = input;
+			document.querySelector('#result').textContent = '정답!'
+			document.querySelector('#input').value = '';
+		}
+		else {
+			document.querySelector('#result').textContent = '땡!'
+		}
+	}
+	else {
+		alert(err);
+	}
 });
